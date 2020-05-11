bc618b844439f182a56091c8657958176df09501
makoto tsuta <makoto1850@gmail.com>
2020-05-11 16:08:32 +0900

    イベントCRUD機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      ContactMailer.contact_mail(@user).deliver  ##追記
+      ContactMailer.contact_mail(@user).deliver
       redirect_to users_path, notice: 'User was successfully created.'
     else
       render :new
