dbcaec93e2e8ae7cffcd8a254e4e945909d7ccd9
makoto tsuta <makoto1850@gmail.com>
2020-05-29 18:08:16 +0900

    ユーザーモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   validates :password,  presence: true, length: { maximum: 30 }
   validates :address,  presence: true, length: { maximum: 255 }
-  validates :icon,  presence: true
+  validates :icon,  presence: true, allow_blank: true
   validates :birthed_at,  presence: true
   validates :name,  presence: true, length: { maximum: 50 }
   validates :sex,  presence: true
